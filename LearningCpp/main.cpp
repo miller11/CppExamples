24a7f136cc3f331088521cdedccafa1069cc061b
miller11 <ross-h-miller@uiowa.edu>
2013-02-19 07:56:46 -0600

    Initial Commit

---
+++ LearningCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LearningCpp
+//
+//  Created by Ross Miller on 2/19/13.
+//  Copyright (c) 2013 Ross Miller. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
